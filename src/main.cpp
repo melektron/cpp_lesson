ac045698c9347ffebfa21e56da2fceaece15c2a1
melektron <matteo@elektron.work>
2023-01-12 17:16:39 +0100

    added hello world program

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+struct mystruct 
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+};
+
+
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    
+
+    return 0;
+}
